96ce63ecc202f370ca29b5c1a74c7877f91f5712
Ketus-home <maciej.oleksa@gmail.com>
2016-03-09 21:27:46 +0100

    change port and address for openshift deployment

--- server.js
+++ server.js
@@ -8,6 +8,4 @@ require('./server/config/mongoose')(config);
 require('./server/config/passport')();
 require('./server/config/routes')(app);
 
-app.listen(config.port, function () {
-    console.log('Server listening on port: ' + config.port);
-});
+app.listen(config.port, config.address);
--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,8 @@ module.exports = {
     development: {
         rootPath: rootPath,
         dbString: 'mongodb://ketus:meanapp@ds041693.mongolab.com:41693/meanappdb',
-        port: process.env.PORT || 1337
+        port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
+        address: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"
     },
     production: {
         rootPath: rootPath,
